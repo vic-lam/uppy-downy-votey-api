7a0396241c6a7ac534def346e482d961a85c219c
davidkorff <davidkorff@gmail.com>
2017-02-17 15:59:42 -0500

    refreshed schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170215154751) do
   enable_extension "plpgsql"
 
   create_table "poll_options", force: :cascade do |t|
-    t.string   "text",       default: " "
+    t.string   "text",       default: "Option"
     t.string   "image",      default: "https://s-media-cache-ak0.pinimg.com/236x/63/43/9b/63439b05d58531785d61655c99907d10.jpg"
     t.integer  "poll_id"
     t.datetime "created_at",                                                                                                     null: false
